5d4d09a0a148713a26d2cca790a0bf0d93fcd310
Luke Woodward <luke.wdwrd@gmail.com>
2017-01-17 17:39:18 -0600

    added  in place of promise

--- app/controllers/bookCtrl.js
+++ app/controllers/bookCtrl.js
@@ -3,10 +3,9 @@
 // guideFactory is injected so this controller can gain access to the returned obj from factory
 app.controller('bookCtrl', function($scope, guideFactory){
     // console.log(guideFactory.getGuides());
-    
+
     guideFactory.getGuides().then(function(resolve){
         $scope.list = resolve
-        $scope.$apply()
         console.log($scope.list, resolve);
     });
 
--- app/factories/guideFactory.js
+++ app/factories/guideFactory.js
@@ -1,11 +1,11 @@
-app.factory('guideFactory', function($http){
+app.factory('guideFactory', function($q, $http){
 
     function getGuides(){
         //returns a promise to use resolve method
-        return new Promise(function(resolve, reject){
+        return $q(function(resolve, reject){
             //calls for the data
             return $http.get(`data/guides.json`)
-            
+
             .then(function(json){
                 let parsedData = json.data.guides
                 resolve(parsedData)
